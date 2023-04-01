c13db141deebfbfd152f2b162f6622be0282030c
Alexander Stefański <alex@aps-studio.eu>
2023-04-01 16:24:12 +0200

    v1.0.3

--- src/types.ts
+++ src/types.ts
@@ -9,7 +9,42 @@ export type StaticRouteBaseGetData<T extends StaticRouteBase<any, any>> = T exte
     ? TData
     : never;
 
+export interface RouteData<T extends StaticRouteBase<any, any>> {
+    data: StaticRouteBaseGetData<T>;
+    htmlData?: HtmlData;
+}
+
 export interface Config<T extends StaticRouteBase<any, any>> {
     routes: T[];
-    getRouteData: (route: T) => Promise<StaticRouteBaseGetData<T>>;
+    getRouteData: (route: T) => Promise<RouteData<T>>;
+    defaultHtmlData?: HtmlData;
+}
+
+export interface HtmlHeadMeta {
+    charset?: string;
+    name?: string;
+    content?: string;
+    httpEquiv?: string;
+    property?: string;
+}
+
+export interface HtmlHeadLink {
+    rel?: string;
+    href?: string;
+    sizes?: string;
+    type?: string;
+    as?: string;
+    crossorigin?: string;
+    fetchpriority?: string;
+}
+
+export interface HtmlHead {
+    meta?: HtmlHeadMeta[];
+    link?: HtmlHeadLink[];
+    title?: string;
+}
+
+export interface HtmlData {
+    lang?: string;
+    head?: HtmlHead;
 }
